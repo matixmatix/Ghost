770d7dd6de750d468b33640899095244cfbd6121
Matic Jurglič <matic@jurglic.si>
2016-05-01 19:37:59 +0200

    don't remove html from excerpt

--- core/server/data/meta/excerpt.js
+++ core/server/data/meta/excerpt.js
@@ -6,8 +6,8 @@ function getExcerpt(html, truncateOptions) {
     var excerpt = html.replace(/<a href="#fn.*?rel="footnote">.*?<\/a>/gi, '');
     excerpt = excerpt.replace(/<div class="footnotes"><ol>.*?<\/ol><\/div>/, '');
     // Strip other html
-    excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
-    excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
+    //excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
+    //excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
     /*jslint regexp:false */
 
     if (!truncateOptions.words && !truncateOptions.characters) {
